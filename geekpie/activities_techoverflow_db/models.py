2b51040b02e9c853582f75e94f42d46f50654218
eastpiger <lvwt@shanghaitech.edu.cn>
2016-04-06 17:55:14 +0800

    update

--- geekpie/activities_techoverflow_db/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class ActivityTechOverflowModel(models.Model):
-    name = models.CharField(max_length=255, default='Name')
-    phone = models.CharField(max_length=255, default='Phone')
-    email = models.CharField(max_length=255, default='Email')
-    target = models.IntegerField(default=1)
